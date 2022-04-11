2d4f103b37ff41d4739a36eeaa022aeb51d56a52
Chase Frankenfeld <chase@frankie.health>
2022-04-11 16:45:47 +1000

    Application skeleton is set up

---
+++ services/talk_booking/tests/integration/test_web_app/test_main.py
@@ -0,0 +1,20 @@
+import pytest
+from starlette.testclient import TestClient
+
+from web_app.main import app
+
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+
+def test_health_check(client):
+    """
+    GIVEN
+    WHEN health check endpoint is called with GET method
+    THEN response with status 200 and body OK is returned
+    """
+    response = client.get("/health-check/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "OK"}
