d49093fa1606991a6c13d47231ab5374168a4990
Chase Frankenfeld <chase@frankie.health>
2022-04-15 17:23:41 +1000

    Add HTTPS certificate

--- infrastructure/modules/talk-booking-service/domain.tf
+++ infrastructure/modules/talk-booking-service/domain.tf
@@ -1,3 +1,30 @@
+resource "aws_acm_certificate" "app" {
+  domain_name       = var.domain
+  validation_method = "DNS"
+}
+
+resource "aws_route53_record" "certificate" {
+  for_each = {
+    for dvo in aws_acm_certificate.app.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  zone_id         = var.domain_zone_id
+  name            = each.value.name
+  type            = each.value.type
+  records         = [each.value.record]
+  ttl             = 60
+}
+
+resource "aws_acm_certificate_validation" "app" {
+  certificate_arn         = aws_acm_certificate.app.arn
+  validation_record_fqdns = [for record in aws_route53_record.certificate : record.fqdn]
+}
+
 resource "aws_route53_record" "app" {
   zone_id = var.domain_zone_id
   name    = var.domain
