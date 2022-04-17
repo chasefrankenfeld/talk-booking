4492fa79799cf6dcc51d6be1cb8772268f6d45be
Chase Frankenfeld <chase@frankie.health>
2022-04-17 15:22:11 +1000

    add e2e tests

--- services/talk_booking/tests/e2e/test_reject_request_flow.py
+++ services/talk_booking/tests/e2e/test_reject_request_flow.py
@@ -1,11 +1,11 @@
 import requests
 
 
-def test_accept_request_flow():
-    api_url = "https://development.beerlibrary.co"
+def test_reject_request_flow():
+    api_url = "https://development.your-domain.com"
     talk_request = request_talk(api_url)
     find_talk_request_in_list(api_url, talk_request)
-    talk_request = accept_talk_request(api_url, talk_request)
+    talk_request = reject_talk_request(api_url, talk_request)
     find_talk_request_in_list(api_url, talk_request)
 
 
@@ -20,7 +20,7 @@ def request_talk(api_url):
                 "state": "Sunny state",
                 "country": "Sunny country",
             },
-            "topic": "FastAPI with Pydantic",
+            "topic": "Complete Python toolbox",
             "duration_in_minutes": 45,
             "requester": "john@doe.com",
         },
@@ -39,11 +39,11 @@ def find_talk_request_in_list(api_url, talk_request):
     ), "Talk request not found in the list."
 
 
-def accept_talk_request(api_url, talk_request):
+def reject_talk_request(api_url, talk_request):
     response = requests.post(
-        f"{api_url}/talk-request/accept/", json={"id": talk_request["id"]}
+        f"{api_url}/talk-request/reject/", json={"id": talk_request["id"]}
     )
-    assert response.status_code == 200, "Something went wrong while accepting a talk."
+    assert response.status_code == 200, "Something went wrong while rejecting a talk."
     talk_request = response.json()
-    assert talk_request["status"] == "ACCEPTED"
+    assert talk_request["status"] == "REJECTED"
     return talk_request
