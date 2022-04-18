cb2ffffd8d616c93d0b385b3342c49c8f73fe4a1
Chase Frankenfeld <chase@frankie.health>
2022-04-19 08:01:50 +1000

    fix flake8 issues and black issues

--- services/talk_booking/web_app/config.py
+++ services/talk_booking/web_app/config.py
@@ -1,6 +1,6 @@
-import os
 import base64
 import json
+import os
 from typing import Optional
 
 import boto3
@@ -16,6 +16,7 @@ class ProductionConfig:
     @property
     def SQLALCHEMY_DATABASE_URI(self) -> str:
         if self._SQLALCHEMY_DATABASE_URI is None:
+            print("SECRET: ", get_secret(self.APP_ENVIRONMENT))
             self._SQLALCHEMY_DATABASE_URI = get_secret(self.APP_ENVIRONMENT)
 
         return self._SQLALCHEMY_DATABASE_URI
@@ -80,46 +81,41 @@ def get_secret(env: str):
 
     # Create a Secrets Manager client
     session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
+    client = session.client(service_name="secretsmanager", region_name=region_name)
 
     # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
     # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
     # We rethrow the exception by default.
 
     try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
     except ClientError as e:
-        if e.response['Error']['Code'] == 'DecryptionFailureException':
+        if e.response["Error"]["Code"] == "DecryptionFailureException":
             # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
             # Deal with the exception here, and/or rethrow at your discretion.
             raise e
-        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+        elif e.response["Error"]["Code"] == "InternalServiceErrorException":
             # An error occurred on the server side.
             # Deal with the exception here, and/or rethrow at your discretion.
             raise e
-        elif e.response['Error']['Code'] == 'Inv salidParameterException':
+        elif e.response["Error"]["Code"] == "Inv salidParameterException":
             # You provided an invalid value for a parameter.
             # Deal with the exception here, and/or rethrow at your discretion.
             raise e
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
+        elif e.response["Error"]["Code"] == "InvalidRequestException":
             # You provided a parameter value that is not valid for the current state of the resource.
             # Deal with the exception here, and/or rethrow at your discretion.
             raise e
-        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+        elif e.response["Error"]["Code"] == "ResourceNotFoundException":
             # We can't find the resource that you asked for.
             # Deal with the exception here, and/or rethrow at your discretion.
             raise e
     else:
         # Decrypts secret using the associated KMS CMK.
         # Depending on whether the secret is a string or binary, one of these fields will be populated.
-        if 'SecretString' in get_secret_value_response:
-            secret = get_secret_value_response['SecretString']
+        if "SecretString" in get_secret_value_response:
+            secret = get_secret_value_response["SecretString"]
         else:
-            secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+            secret = base64.b64decode(get_secret_value_response["SecretBinary"])
 
     return json.loads(secret)
