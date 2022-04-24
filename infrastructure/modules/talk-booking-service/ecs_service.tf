fcd32fe266d637a6938bf20b09eeb1bd023efc7e
Chase Frankenfeld <chase@frankie.health>
2022-04-25 08:34:14 +1000

    adjust

--- infrastructure/modules/talk-booking-service/ecs_service.tf
+++ infrastructure/modules/talk-booking-service/ecs_service.tf
@@ -14,7 +14,7 @@ resource "aws_ecs_task_definition" "app" {
       "environment": [
         {"name": "APP_ENVIRONMENT", "value": "${var.app_environment}"},
         {"name": "AWS_DEFAULT_REGION", "value": "${var.region}"}
-        {"name": "PORT", "value": "${var.container_port}"}
+        {"name": "PORT", "value": ${var.container_port}}
       ],
       "portMappings": [
         {
